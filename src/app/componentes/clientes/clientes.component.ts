fbe574805539eb6bff7bb304aa8cc8f74efd5ec0
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-12-15 19:21:27 -0500

    listando los usuarios en la tabla

--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
 
 @Component({
   selector: 'app-clientes',
@@ -6,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit {
+
+  clientes: Cliente[] = [
+    {
+      id: 1,
+      nombres: 'Braulio Fernando',
+      apellidos: 'Cusco Mejia',
+      email: 'admin@admin.com',
+      createAt: '2019-12-15'
+    },
+    {
+      id: 2,
+      nombres: 'Braulio Fernando',
+      apellidos: 'Cusco Mejia',
+      email: 'admin@admin.com',
+      createAt: '2019-12-15'
+    },
+    {
+      id: 3,
+      nombres: 'Braulio Fernando',
+      apellidos: 'Cusco Mejia',
+      email: 'admin@admin.com',
+      createAt: '2019-12-15'
+    },
+    {
+      id: 4,
+      nombres: 'Braulio Fernando',
+      apellidos: 'Cusco Mejia',
+      email: 'admin@admin.com',
+      createAt: '2019-12-15'
+    }
+  ]
 
   constructor() { }
 
