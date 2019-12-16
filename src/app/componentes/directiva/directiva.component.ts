d045de87e32a6c58c75cd2445041d7a87e089310
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-12-15 19:05:07 -0500

    directiva ngIf

--- src/app/componentes/directiva/directiva.component.ts
+++ src/app/componentes/directiva/directiva.component.ts
@@ -9,9 +9,24 @@ export class DirectivaComponent implements OnInit {
 
   listaCurso: string[] = ['Java', 'Python', 'Php', 'Ionic',  'Angular']
 
+  habilitar: boolean = true;
+  c: number = 1;
+  valorBtn: string = "Ocultar"
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  show() {
+    if(this.c%2 == 0) {
+      this.habilitar = true;
+      this.valorBtn = "Ocultar";
+    } else {
+      this.habilitar = false;
+      this.valorBtn = "Mostrar";
+    }
+    this.c++;
   }
 
 }
