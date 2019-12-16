10ea59a714fa0b4ff45c86695bb77ffef3a4e361
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-12-15 19:09:03 -0500

    creando el componente cliente y la clase cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './componentes/nav/nav.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { DirectivaComponent } from './componentes/directiva/directiva.component';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     FooterComponent,
-    DirectivaComponent
+    DirectivaComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule
