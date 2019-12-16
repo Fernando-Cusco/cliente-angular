1e0a0a99f547d445558e6a1cb27d7c33b793c79e
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-12-15 19:24:44 -0500

    separando lista de clientes en un archivo externo

--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -8,40 +9,12 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    {
-      id: 1,
-      nombres: 'Braulio Fernando',
-      apellidos: 'Cusco Mejia',
-      email: 'admin@admin.com',
-      createAt: '2019-12-15'
-    },
-    {
-      id: 2,
-      nombres: 'Braulio Fernando',
-      apellidos: 'Cusco Mejia',
-      email: 'admin@admin.com',
-      createAt: '2019-12-15'
-    },
-    {
-      id: 3,
-      nombres: 'Braulio Fernando',
-      apellidos: 'Cusco Mejia',
-      email: 'admin@admin.com',
-      createAt: '2019-12-15'
-    },
-    {
-      id: 4,
-      nombres: 'Braulio Fernando',
-      apellidos: 'Cusco Mejia',
-      email: 'admin@admin.com',
-      createAt: '2019-12-15'
-    }
-  ]
+  clientes: Cliente[];
 
   constructor() { }
 
   ngOnInit() {
+    this.clientes = CLIENTES;
   }
 
 }
---
+++ src/app/componentes/clientes/clientes.json.ts
@@ -0,0 +1,32 @@
+import { Cliente } from './cliente';
+export const CLIENTES: Cliente[] = [
+
+{
+    id: 1,
+    nombres: 'Braulio Fernando',
+    apellidos: 'Cusco Mejia',
+    email: 'admin@admin.com',
+    createAt: '2019-12-15'
+  },
+  {
+    id: 2,
+    nombres: 'Braulio Fernando',
+    apellidos: 'Cusco Mejia',
+    email: 'admin@admin.com',
+    createAt: '2019-12-15'
+  },
+  {
+    id: 3,
+    nombres: 'Braulio Fernando',
+    apellidos: 'Cusco Mejia',
+    email: 'admin@admin.com',
+    createAt: '2019-12-15'
+  },
+  {
+    id: 4,
+    nombres: 'Braulio Fernando',
+    apellidos: 'Cusco Mejia',
+    email: 'admin@admin.com',
+    createAt: '2019-12-15'
+  }
+]
